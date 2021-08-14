d8e7f779ab51f221936123086a3c5fcf3c628413
AalaaEid <aalaa@ryets.com>
2021-08-14 10:15:11 +0200

    test changes

--- Example-Swift/FSCalendarSwiftExample/LunarFormatter.swift
+++ Example-Swift/FSCalendarSwiftExample/LunarFormatter.swift
@@ -14,7 +14,7 @@ open class LunarFormatter: NSObject {
     fileprivate let formatter = DateFormatter()
     fileprivate let lunarDays = ["初二","初三","初四","初五","初六","初七","初八","初九","初十","十一","十二","十三","十四","十五","十六","十七","十八","十九","二十","二一","二二","二三","二四","二五","二六","二七","二八","二九","三十"]
     fileprivate let lunarMonths = ["正月","二月","三月","四月","五月","六月","七月","八月","九月","十月","冬月","腊月"]
-    
+    fileprivate let lMonths = ["正月","二月","三月","四月","五月","六月","七月","八月","九月","十月","冬月","腊月"]
     
     override init() {
         self.formatter.calendar = self.chineseCalendar
